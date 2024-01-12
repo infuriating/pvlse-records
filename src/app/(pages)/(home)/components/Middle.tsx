bfc6803edbdb219a8b77ba674cf932d40a768e29
infuriating <decimatedxd@gmail.com>
2024-01-12 16:52:29 +0100

    Update LatestReleases component import path

--- src/app/(pages)/(home)/components/Middle.tsx
+++ src/app/(pages)/(home)/components/Middle.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Hero from "./content/Hero";
 import FeaturedArtist from "./content/FeaturedArtist";
-import LatestReleases from "./content/LatestReleases";
+import LatestReleases from "./content/latest-releases/LatestReleases";
 
 export default function Middle() {
   return (
-    <div className="grid grid-cols-2 grid-rows-4 pt-6 px-8">
+    <div className="md:grid grid-cols-2 grid-rows-4 pt-6 px-8">
       <div className="col-span-1 row-span-2 pt-8">
         <Hero />
       </div>
       <div className="col-span-1 row-span-4">
         <FeaturedArtist />
       </div>
-      <div className="col-span-1 row-span-2">
+      <div className="col-span-1 row-span-2 flex justify-center">
         <LatestReleases />
       </div>
     </div>
