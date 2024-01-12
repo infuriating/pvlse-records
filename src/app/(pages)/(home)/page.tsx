4bbd2c1b09886530fbe2a74e343a6efe84e6b451
inf <decimatedxd@gmail.com>
2024-01-12 20:59:10 +0100

    Add Header component to RootLayout

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,11 +1,9 @@
 import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Middle from "./components/Middle";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Middle />
       <Footer />
     </>
