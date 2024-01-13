01825c354aeae9ab58d9ebafb68be83f8e8cea05
inf <decimatedxd@gmail.com>
2024-01-13 15:24:43 +0100

    Removed middle to clean up tree, put it on page

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,11 +1,22 @@
-import Footer from "./components/Footer";
-import Middle from "./components/Middle";
+import React from "react";
+import Hero from "./components/content/Hero";
+import FeaturedArtist from "./components/content/FeaturedArtist";
+import LatestReleases from "./components/content/latest-releases/LatestReleases";
 
-export default function Home() {
+export default function page() {
   return (
-    <>
-      <Middle />
-      <Footer />
-    </>
+    <div className="flex justify-center pt-8">
+      <div className="flex flex-col md:grid grid-cols-2 grid-rows-4 px-8 max-w-[1600px]">
+        <div className="col-span-1 row-span-2 lg:pt-4">
+          <Hero />
+        </div>
+        <div className="col-span-1 row-span-4 lg:pt-4">
+          <FeaturedArtist />
+        </div>
+        <div className="col-span-1 row-span-2 flex justify-center pb-8 lg:pb-0 lg:justify-start">
+          <LatestReleases />
+        </div>
+      </div>
+    </div>
   );
 }
