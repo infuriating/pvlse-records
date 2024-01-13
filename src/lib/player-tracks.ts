1cd324b27cf36b769711768a224c32bc45eb7377
inf <decimatedxd@gmail.com>
2024-01-13 19:50:50 +0100

    Add new track images and update player tracks

--- src/lib/player-tracks.ts
+++ src/lib/player-tracks.ts
@@ -3,31 +3,34 @@ interface PlayerTrack {
   artists: string[];
   genre: string;
   filePath: string;
+  coverImage?: string;
 }
 
 export const PlayerTracks: PlayerTrack[] = [
-  // {
-  //   title: "nevoa",
-  //   artists: ["dashie", "ISXRO"],
-  //   genre: "krushphonk",
-  //   filePath: "nevoa.mp3",
-  // },
-  // {
-  //   title: "digiphunk",
-  //   artists: ["dashie", "ISXRO"],
-  //   genre: "digital phonk",
-  //   filePath: "digiphunk.mp3",
-  // },
+  {
+    title: "nevoa",
+    artists: ["dashie", "ISXRO"],
+    genre: "krushphonk",
+    filePath: "nevoa.mp3",
+  },
+  {
+    title: "digiphunk",
+    artists: ["dashie", "ISXRO"],
+    genre: "digital phonk",
+    filePath: "digiphunk.mp3",
+  },
   {
     title: "everybody Is ##starryEyed !",
     artists: ["hatena"],
     genre: "dariacore",
     filePath: "starryeyed.mp3",
+    coverImage: "starryeyed.png",
   },
   {
     title: "evergreen",
     artists: ["dashie"],
     genre: "rap",
     filePath: "evergreen.mp3",
+    coverImage: "evergreen.png",
   },
 ];
