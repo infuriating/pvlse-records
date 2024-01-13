98b784de95c57247c378528718bd7ed4d943173b
inf <decimatedxd@gmail.com>
2024-01-13 02:39:41 +0100

    Add player tracks interface and data

---
+++ src/lib/player-tracks.ts
@@ -0,0 +1,18 @@
+interface PlayerTrack {
+  title: string;
+  artists: string[];
+  filePath: string;
+}
+
+export const PlayerTracks: PlayerTrack[] = [
+  {
+    title: "nevoa",
+    artists: ["dashie", "ISXRO"],
+    filePath: "nevoa.mp3",
+  },
+  {
+    title: "digiphunk",
+    artists: ["dashie", "ISXRO"],
+    filePath: "digiphunk.mp3",
+  },
+];
