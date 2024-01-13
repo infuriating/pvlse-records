856b13d1129cf298c6471a4fc31054b9688c3b38
inf <decimatedxd@gmail.com>
2024-01-13 14:30:18 +0100

    Refactor layout in Middle component

--- src/app/(pages)/(home)/components/Middle.tsx
+++ src/app/(pages)/(home)/components/Middle.tsx
@@ -5,15 +5,17 @@ import LatestReleases from "./content/latest-releases/LatestReleases";
 
 export default function Middle() {
   return (
-    <div className="flex flex-col items-center md:grid grid-cols-2 grid-rows-4 px-8">
-      <div className="col-span-1 row-span-2 pt-4 lg:pt-8">
-        <Hero />
-      </div>
-      <div className="col-span-1 row-span-4">
-        <FeaturedArtist />
-      </div>
-      <div className="col-span-1 row-span-2 flex justify-center pb-8 lg:pb-0 lg:justify-start">
-        <LatestReleases />
+    <div className="flex justify-center">
+      <div className="flex flex-col md:grid grid-cols-2 grid-rows-4 px-8 max-w-[1600px]">
+        <div className="col-span-1 row-span-2 pt-4 lg:pt-8">
+          <Hero />
+        </div>
+        <div className="col-span-1 row-span-4 pt-12">
+          <FeaturedArtist />
+        </div>
+        <div className="col-span-1 row-span-2 flex justify-center pb-8 lg:pb-0 lg:justify-start">
+          <LatestReleases />
+        </div>
       </div>
     </div>
   );
