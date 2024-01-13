30acdd69c38e35472426e162ddeaec28374d9393
inf <decimatedxd@gmail.com>
2024-01-13 02:46:45 +0100

    Add new track and update track information

--- src/lib/player-tracks.ts
+++ src/lib/player-tracks.ts
@@ -5,14 +5,19 @@ interface PlayerTrack {
 }
 
 export const PlayerTracks: PlayerTrack[] = [
+  // {
+  //   title: "nevoa",
+  //   artists: ["dashie", "ISXRO"],
+  //   filePath: "nevoa.mp3",
+  // },
+  // {
+  //   title: "digiphunk",
+  //   artists: ["dashie", "ISXRO"],
+  //   filePath: "digiphunk.mp3",
+  // },
   {
-    title: "nevoa",
-    artists: ["dashie", "ISXRO"],
-    filePath: "nevoa.mp3",
-  },
-  {
-    title: "digiphunk",
-    artists: ["dashie", "ISXRO"],
-    filePath: "digiphunk.mp3",
+    title: "everybody Is ##starryEyed !",
+    artists: ["hatena"],
+    filePath: "starryeyed.mp3",
   },
 ];
