d59e74bd5e5c69a3f032973c7d41f321dfe71a41
inf <decimatedxd@gmail.com>
2024-01-13 14:51:06 +0100

    Update Hero component styling

--- src/app/(pages)/(home)/components/content/Hero.tsx
+++ src/app/(pages)/(home)/components/content/Hero.tsx
@@ -4,10 +4,10 @@ export default function Hero() {
   return (
     <>
       <p className="text-xs">##feelthepvlse</p>
-      <p className="text-4xl font-semibold">
+      <p className="text-4xl font-semibold w-max pb-1 border-b">
         We are <span className="font-black">PVLSE</span>
       </p>
-      <p className="text-base max-w-[650px]">
+      <p className="text-base max-w-[650px] pt-1">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
