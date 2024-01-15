78d9ccee63879aec484d61d554826d81ca038701
infuriating <decimatedxd@gmail.com>
2024-01-15 12:03:16 +0100

    Add dynamic rendering of featured artist and their social media links

--- src/app/(pages)/(home)/components/content/FeaturedArtist.tsx
+++ src/app/(pages)/(home)/components/content/FeaturedArtist.tsx
@@ -1,27 +1,50 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { artists } from "@/lib/artists";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faSoundcloud } from "@fortawesome/free-brands-svg-icons";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export default function FeaturedArtist() {
+  const artist = artists[Math.floor(Math.random() * artists.length)];
+
   return (
     <div className="grid place-items-center pt-8 md:pt-0 md:place-items-end w-full pr-12">
       <p className="text-sm font-bold">Featured Artist</p>
       <div className="border mt-2 p-4">
-        <div className="border bg-gray-200 h-60 xl:h-96 aspect-square rounded-lg overflow-hidden object-cover">
-          <Image src="/dashie.png" alt="Dashie" height={384} width={384} />
+        <div className="border h-60 xl:h-96 aspect-square rounded-lg overflow-hidden object-cover">
+          {artist.image ? (
+            <Image
+              src={`/${artist.image}`}
+              alt={artist.name}
+              height={384}
+              width={384}
+            />
+          ) : (
+            <Skeleton className="h-full" />
+          )}
         </div>
-        <p className="font-bold text-lg pt-2 text-center">dashie</p>
+        <p className="font-bold text-lg pt-2 text-center">{artist.name}</p>
         <div className="flex justify-center gap-x-4 mt-2">
-          <Link href="#">
-            <Skeleton className="border h-12 aspect-square bg-muted rounded-full" />
-          </Link>
-          <Link href="#">
-            <Skeleton className="border h-12 aspect-square bg-muted rounded-full" />
-          </Link>
-          <Link href="#">
-            <Skeleton className="border h-12 aspect-square bg-muted rounded-full" />
-          </Link>
+          {artist.socials.map((social) => (
+            <Link key={social} href={social} target="_blank">
+              <div className="bg-muted border h-12 aspect-square rounded-full flex justify-center items-center">
+                {social.includes("twitter") && (
+                  <FontAwesomeIcon icon={faXTwitter} className="h-6 w-6" />
+                )}
+                {social.includes("instagram") && (
+                  <FontAwesomeIcon icon={faInstagram} className="h-6 w-6" />
+                )}
+                {social.includes("soundcloud") && (
+                  <FontAwesomeIcon icon={faSoundcloud} className="h-6 w-6" />
+                )}
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
