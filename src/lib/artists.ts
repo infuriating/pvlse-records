9469dbfbf485a4e3d0d13f3baddd1a268febeaf4
infuriating <decimatedxd@gmail.com>
2024-01-15 12:03:19 +0100

    Update artists interface and data

--- src/lib/artists.ts
+++ src/lib/artists.ts
@@ -1,11 +1,27 @@
 interface Artist {
   name: string;
-  //   url: string;
-  //   cover: string;
+  description: string;
+  socials: string[];
+  spotifyURL?: string;
+  image?: string;
 }
 
-export const Artists: Artist[] = [
+export const artists: Artist[] = [
   {
     name: "dashie",
+    description:
+      "dashie is a artist from Aberdeen, Scotland. He makes mindblowing tracks surrounding genres such as hyperpop, dariacore and brazillian phonk.",
+    socials: ["https://instagram.com/1dshi", "https://soundcloud.com/dshie"],
+    spotifyURL:
+      "https://open.spotify.com/artist/6Yq4YUy8Xp1o2x0f9tU9ZD?si=8f0b4d6a6e8d4b0a",
+    image: "dashie.png",
+  },
+  {
+    name: "inf",
+    description: "inf.",
+    socials: [
+      "https://instagram.com/infs.world",
+      "https://twitter.com/xnfuriating",
+    ],
   },
 ];
