4341cdf34cc08a6983c02809a51891112ad63762
inf <decimatedxd@gmail.com>
2024-01-15 16:44:59 +0100

    Refactor artist loading component

--- src/app/(pages)/artists/[artist]/loading.tsx
+++ src/app/(pages)/artists/[artist]/loading.tsx
@@ -4,7 +4,7 @@ export default function loading() {
   return (
     <div className="flex justify-center pt-12">
       <div className="p-2 rounded-lg bg-primary-foreground aspect-[1.4/1] min-w-80 max-w-[500px] flex justify-start items-start flex-col">
-        <Skeleton className="w-12 h-8" />
+        <Skeleton className="w-12 h-4 pb-2" />
         <Skeleton className="w-32 h-2 pt-2 pb-2 text-muted-foreground" />
         <div className="flex w-full justify-center">
           <div className="border h-24 xl:h-32 aspect-square rounded-lg overflow-hidden object-cover">
