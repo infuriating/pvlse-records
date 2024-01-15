fb4d6701d925b852d9b7d43bdb32fb14a367c87b
inf <decimatedxd@gmail.com>
2024-01-15 15:31:03 +0100

    Update Track interface and add new properties
    This commit updates the Track interface in the tracks.ts file to change the "artist" property to "artists" as an array. It also adds new optional properties "url" and "coverImage" to the Track interface.

--- src/lib/tracks.ts
+++ src/lib/tracks.ts
@@ -1,21 +1,23 @@
 interface Track {
-  artist: string;
+  artists: string[];
   title: string;
-  //   url: string;
-  //   cover: string;
+  url?: string;
+  coverImage?: string;
 }
 
 export const Tracks: Track[] = [
   {
-    artist: "dashie",
+    artists: ["dashie", "ISXRO"],
     title: "ultraphunk",
+    url: "https://open.spotify.com/track/2rCVGFo2htxWlkDwm3klAB?si=c5a7687c13c04643",
+    coverImage: "ultraphunk.png",
   },
   {
-    artist: "dashie",
+    artists: ["dashie", "ISXRO"],
     title: "orquestra phunka",
   },
   {
-    artist: "dashie",
+    artists: ["dashie", "ISXRO"],
     title: "nevoa",
   },
 ];
