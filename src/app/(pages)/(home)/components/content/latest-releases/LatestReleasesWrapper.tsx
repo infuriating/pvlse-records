03f800a28adc0123b0f3daec61d762bb2344dd90
infuriating <decimatedxd@gmail.com>
2024-01-18 09:13:35 +0100

    Update LatestReleases to use getLatestFour API

--- src/app/(pages)/(home)/components/content/latest-releases/LatestReleasesWrapper.tsx
+++ src/app/(pages)/(home)/components/content/latest-releases/LatestReleasesWrapper.tsx
@@ -3,6 +3,6 @@ import { api } from "../../../../../../../convex/_generated/api";
 import LatestReleases from "./LatestReleases";
 
 export async function LatestReleasesWrapper() {
-  const preloadedTasks = await preloadQuery(api.tracks.getAll);
+  const preloadedTasks = await preloadQuery(api.tracks.getLatestFour);
   return <LatestReleases preloadedTasks={preloadedTasks} />;
 }
