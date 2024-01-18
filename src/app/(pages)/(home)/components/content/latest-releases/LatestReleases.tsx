de47efcaa698665007a57ef47664c4e44ab0a61d
infuriating <decimatedxd@gmail.com>
2024-01-18 09:28:52 +0100

    Refactor LatestReleases component to remove unnecessary sorting and slicing

--- src/app/(pages)/(home)/components/content/latest-releases/LatestReleases.tsx
+++ src/app/(pages)/(home)/components/content/latest-releases/LatestReleases.tsx
@@ -11,18 +11,13 @@ export default function LatestReleases(props: {
   const tracks = usePreloadedQuery(props.preloadedTasks);
   if (!tracks) return <></>;
 
-  const latestTracks = tracks.sort((a, b) => {
-    return b._creationTime - a._creationTime;
-  });
-  const latestFourTracks = latestTracks.slice(0, 4);
-
   return (
     <div className="pt-8 md:pt-4">
       <p className="text-sm rounded-t-sm px-2 py-1 font-bold bg-primary w-max text-secondary">
         Latest Releases
       </p>
       <div className="grid grid-cols-2 gap-y-6 lg:gap-y-0 lg:grid-cols-4 w-max gap-x-6 p-4 bg-muted rounded-b-md">
-        {latestFourTracks.map((track) => {
+        {tracks.map((track) => {
           return (
             <Track
               key={track._id}
