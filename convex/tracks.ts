4ed555fec0441caac9502db75d023574f9424bbb
infuriating <decimatedxd@gmail.com>
2024-01-18 09:13:25 +0100

    Add getLatestFour query to tracks.ts

--- convex/tracks.ts
+++ convex/tracks.ts
@@ -6,3 +6,11 @@ export const getAll = query({
     return await ctx.db.query("track").collect();
   },
 });
+
+export const getLatestFour = query({
+  args: {},
+  handler: async (ctx) => {
+    const tasks = await ctx.db.query("track").order("desc").take(4);
+    return tasks;
+  },
+});
