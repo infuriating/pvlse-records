b4a3eba78132ea12fe4e69c505eb1d5424950e2c
inf <decimatedxd@gmail.com>
2024-01-19 21:38:43 +0100

    Add mutation for adding a track

--- convex/tracks.ts
+++ convex/tracks.ts
@@ -1,4 +1,5 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 export const getAll = query({
   args: {},
@@ -11,5 +12,24 @@ export const getLatestFour = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("track").order("desc").take(4);
+  },
+});
+
+export const addTrack = mutation({
+  args: {
+    artists: v.array(v.string()),
+    title: v.string(),
+    coverImage: v.optional(v.string()),
+    url: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const track = await ctx.db.insert("track", {
+      artists: args.artists,
+      title: args.title,
+      coverImage: args.coverImage,
+      url: args.url,
+    });
+
+    return track;
   },
 });
