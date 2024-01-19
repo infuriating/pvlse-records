a239c963020dbc037c6f8ced9279fa007c88aa8e
inf <decimatedxd@gmail.com>
2024-01-19 21:15:55 +0100

    Add support for YouTube social icon

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -1,25 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faXTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faSoundcloud } from "@fortawesome/free-brands-svg-icons";
 import React from "react";
 
 export default function SocialIcon({ social }: { social: string }) {
   return (
-    <>
-      {social !== "" && (
-        <div className="bg-muted hover:bg-muted-foreground transition-all border h-12 aspect-square rounded-full flex justify-center items-center">
-          {social.includes("twitter") && (
-            <FontAwesomeIcon icon={faXTwitter} className="h-6 w-6" />
-          )}
-          {social.includes("instagram") && (
-            <FontAwesomeIcon icon={faInstagram} className="h-6 w-6" />
-          )}
-          {social.includes("soundcloud") && (
-            <FontAwesomeIcon icon={faSoundcloud} className="h-6 w-6" />
-          )}
-        </div>
+    <div className="bg-muted hover:bg-muted-foreground transition-all border h-12 aspect-square rounded-full flex justify-center items-center">
+      {social.includes("twitter") && (
+        <FontAwesomeIcon icon={faXTwitter} className="h-6 w-6" />
       )}
-    </>
+      {social.includes("instagram") && (
+        <FontAwesomeIcon icon={faInstagram} className="h-6 w-6" />
+      )}
+      {social.includes("soundcloud") && (
+        <FontAwesomeIcon icon={faSoundcloud} className="h-6 w-6" />
+      )}
+      {social.includes("youtube") && (
+        <FontAwesomeIcon icon={faYoutube} className="h-6 w-6" />
+      )}
+    </div>
   );
 }
