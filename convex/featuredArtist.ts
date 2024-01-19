b699c1b1337aa12b592a90d6c0e1ba2037f1dedb
inf <decimatedxd@gmail.com>
2024-01-19 22:15:45 +0100

    Add mutation to update featured artist

--- convex/featuredArtist.ts
+++ convex/featuredArtist.ts
@@ -1,4 +1,4 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const getFeaturedArtist = query({
   args: {},
@@ -6,3 +6,31 @@ export const getFeaturedArtist = query({
     return await ctx.db.query("featured_artist").collect();
   },
 });
+
+export const updateFeaturedArtist = mutation(
+  async (
+    { db },
+    {
+      name,
+      socials,
+      image,
+    }: {
+      name: string;
+      socials: string[];
+      image: string | undefined;
+    }
+  ) => {
+    const document = await db
+      .query("featured_artist")
+      .filter((q) => q.eq(q.field("name"), name))
+      .first();
+
+    if (!document) return;
+
+    document.name = name;
+    document.socials = socials;
+    document.image = image;
+
+    await db.replace(document._id, document);
+  }
+);
