bc0c8d3826928deda8da257cb064b66343a56e41
inf <decimatedxd@gmail.com>
2024-01-19 23:06:52 +0100

    added add, edit and delete mutations for playerTracks

--- convex/playerTracks.ts
+++ convex/playerTracks.ts
@@ -1,4 +1,5 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 export const getAll = query({
   args: {},
@@ -6,3 +7,71 @@ export const getAll = query({
     return await ctx.db.query("player_track").collect();
   },
 });
+
+export const addPlayerTrack = mutation({
+  args: {
+    artists: v.array(v.string()),
+    coverImage: v.optional(v.string()),
+    filePath: v.string(),
+    genre: v.string(),
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const playerTrack = await ctx.db.insert("player_track", {
+      artists: args.artists,
+      coverImage: args.coverImage,
+      filePath: args.filePath,
+      genre: args.genre,
+      title: args.title,
+    });
+
+    return playerTrack;
+  },
+});
+
+export const editPlayerTrack = mutation(
+  async (
+    { db },
+    {
+      artists,
+      coverImage,
+      filePath,
+      genre,
+      title,
+    }: {
+      artists: string[];
+      coverImage: string | undefined;
+      filePath: string;
+      genre: string;
+      title: string;
+    }
+  ) => {
+    const document = await db
+      .query("player_track")
+      .filter((q) => q.eq(q.field("title"), title))
+      .first();
+
+    if (!document) return;
+
+    document.artists = artists;
+    document.coverImage = coverImage;
+    document.filePath = filePath;
+    document.genre = genre;
+    document.title = title;
+
+    await db.replace(document._id, document);
+  }
+);
+
+export const deletePlayerTrack = mutation(
+  async ({ db }, { title }: { title: string }) => {
+    const document = await db
+      .query("player_track")
+      .filter((q) => q.eq(q.field("title"), title))
+      .first();
+
+    if (!document) return;
+
+    await db.delete(document._id);
+  }
+);
