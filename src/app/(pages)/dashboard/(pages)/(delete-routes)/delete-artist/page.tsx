6c83bac4bd5301d478189d299f8f3fa5e61e015c
inf <decimatedxd@gmail.com>
2024-01-19 22:37:38 +0100

    Rename EditArtistWrapper to DeleteArtistWrapper

--- src/app/(pages)/dashboard/(pages)/(delete-routes)/delete-artist/page.tsx
+++ src/app/(pages)/dashboard/(pages)/(delete-routes)/delete-artist/page.tsx
@@ -2,7 +2,7 @@ import { preloadQuery } from "convex/nextjs";
 import { api } from "../../../../../../../convex/_generated/api";
 import DeleteArtist from "./components/DeleteArtist";
 
-export default async function EditArtistWrapper() {
+export default async function DeleteArtistWrapper() {
   const preloadedTasks = await preloadQuery(api.artists.getAll);
   return <DeleteArtist preloadedTasks={preloadedTasks} />;
 }
