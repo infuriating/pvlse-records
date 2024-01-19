f5639279b614517b5f03cbfe787894cd547845d3
infuriating <decimatedxd@gmail.com>
2024-01-19 10:46:38 +0100

    Refactor FeaturedArtist component and use api.featuredArtist.getFeaturedArtist

--- src/app/(pages)/(home)/components/content/featured-artist/FeaturedArtist.tsx
+++ src/app/(pages)/(home)/components/content/featured-artist/FeaturedArtist.tsx
@@ -9,29 +9,31 @@ import { Preloaded, usePreloadedQuery } from "convex/react";
 import { api } from "../../../../../../../convex/_generated/api";
 
 export default function FeaturedArtist(props: {
-  preloadedTasks: Preloaded<typeof api.artists.getAll>;
+  preloadedTasks: Preloaded<typeof api.featuredArtist.getFeaturedArtist>;
 }) {
-  const artists = usePreloadedQuery(props.preloadedTasks);
-  if (!artists) return <></>;
+  const featuredArtist = usePreloadedQuery(props.preloadedTasks);
+  if (!featuredArtist) return <></>;
 
-  const artist = artists[Math.floor(Math.random() * artists.length)];
+  const artist = featuredArtist[0];
 
   return (
     <div className="grid place-items-center pt-8 md:pt-0 md:place-items-end w-full pr-12">
       <p className="text-sm font-bold">Featured Artist</p>
       <div className="border mt-2 p-4">
-        <div className="border h-60 xl:h-96 aspect-square rounded-lg overflow-hidden object-cover">
-          {artist.image ? (
-            <Image
-              src={artist.image}
-              alt={artist.name}
-              height={384}
-              width={384}
-            />
-          ) : (
-            <Skeleton className="h-full" />
-          )}
-        </div>
+        <Link href={`artists/${artist.name}`}>
+          <div className="border h-60 xl:h-96 aspect-square rounded-lg overflow-hidden object-cover">
+            {artist.image ? (
+              <Image
+                src={artist.image}
+                alt={artist.name}
+                height={384}
+                width={384}
+              />
+            ) : (
+              <Skeleton className="h-full" />
+            )}
+          </div>
+        </Link>
         <p className="font-bold text-lg pt-2 text-center">{artist.name}</p>
         <div className="flex justify-center gap-x-4 mt-2">
           {artist.socials.map((social) => (
--- src/app/(pages)/(home)/components/content/featured-artist/FeaturedArtistWrapper.tsx
+++ src/app/(pages)/(home)/components/content/featured-artist/FeaturedArtistWrapper.tsx
@@ -3,6 +3,8 @@ import FeaturedArtist from "./FeaturedArtist";
 import { api } from "../../../../../../../convex/_generated/api";
 
 export async function FeaturedArtistWrapper() {
-  const preloadedTasks = await preloadQuery(api.artists.getAll);
+  const preloadedTasks = await preloadQuery(
+    api.featuredArtist.getFeaturedArtist
+  );
   return <FeaturedArtist preloadedTasks={preloadedTasks} />;
 }
