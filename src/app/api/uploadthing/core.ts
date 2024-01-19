2a913c309b353a5bb69634c6b850320d29e35dfe
inf <decimatedxd@gmail.com>
2024-01-19 20:49:03 +0100

    Add uploadthing functionality

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,26 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "1MB" } })
+    .middleware(async ({ req }) => {
+      return { userId: "123" };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+      return { uploadedBy: metadata.userId };
+    }),
+  audioUploader: f({ audio: { maxFileSize: "8MB" } })
+    .middleware(async ({ req }) => {
+      return { userId: "123" };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
