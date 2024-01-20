ec542aabbfb64a1a6d8a173d4f2c54f748cd9c55
inf <decimatedxd@gmail.com>
2024-01-20 12:36:10 +0100

    Refactor Tracks component to use Card component

--- src/app/(pages)/tracks/components/Tracks.tsx
+++ src/app/(pages)/tracks/components/Tracks.tsx
@@ -6,6 +6,8 @@ import { api } from "../../../../../convex/_generated/api";
 import Image from "next/image";
 import { Skeleton } from "@/components/ui/skeleton";
 import Link from "next/link";
+import { CardContent, CardFooter, Card } from "@/components/ui/card";
+import { ArrowRightCircleIcon } from "lucide-react";
 
 export default function Tracks(params: {
   preloadedTasks: Preloaded<typeof api.tracks.getAll>;
@@ -15,13 +17,9 @@ export default function Tracks(params: {
   return (
     <div className="grid place-items-center grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 pt-6 px-8 gap-x-6 gap-y-8">
       {tracks.map((track) => (
-        <Link
-          className="min-w-60 lg:min-w-72"
-          href={`/tracks/${track.title}`}
-          key={track.title}
-        >
-          <div className="bg-primary-foreground rounded-lg p-4">
-            <div className="w-full flex justify-center">
+        <Link href={`/tracks/${track.title}`} key={track.title}>
+          <Card>
+            <CardContent className="pt-4">
               {track.coverImage ? (
                 <Image
                   className="h-48 w-48 lg:h-60 lg:w-60 object-cover border"
@@ -33,20 +31,30 @@ export default function Tracks(params: {
               ) : (
                 <Skeleton className="h-48 w-48 lg:h-60 lg:w-60 border" />
               )}
-            </div>
+            </CardContent>
 
-            <p className="mt-2 text-2xl font-bold">{track.title}</p>
-            <p className="text-muted-foreground">
-              {track.artists
-                .filter((artist) => artist)
-                .map((artist, index, self) => (
-                  <span key={index}>
-                    {artist}
-                    {index !== self.length - 1 && ", "}
-                  </span>
-                ))}
-            </p>
-          </div>
+            <CardFooter className="flex items-center justify-between">
+              <div>
+                <p className="text-xl font-bold">{track.title}</p>
+                <p className="text-muted-foreground">
+                  {track.artists
+                    .filter((artist) => artist)
+                    .map((artist, index, self) => (
+                      <span key={index}>
+                        {artist}
+                        {index !== self.length - 1 && ", "}
+                      </span>
+                    ))}
+                </p>
+                <p className="text-xs text-muted-foreground">
+                  {track.genre.toUpperCase()}
+                </p>
+              </div>
+              <div>
+                <ArrowRightCircleIcon className="h-8 w-8 text-muted-foreground transition-all hover:text-primary" />
+              </div>
+            </CardFooter>
+          </Card>
         </Link>
       ))}
     </div>
