5b02f1f92d5eb951913fa05bba4d8e46ab24ac34
inf <decimatedxd@gmail.com>
2024-01-20 11:29:31 +0100

    Remove unnecessary headings in DashboardRoutes.tsx

--- src/app/(pages)/dashboard/components/DashboardRoutes.tsx
+++ src/app/(pages)/dashboard/components/DashboardRoutes.tsx
@@ -9,7 +9,6 @@ export default function DashboardRoutes() {
     <main className="flex-1 overflow-auto p-6">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <section>
-          <h3 className="text-lg font-semibold mb-4">Add</h3>
           {addRoutes.map((route) => (
             <Link key={route.name} href={route.path}>
               <div className="mb-6 flex flex-col items-center justify-center p-4 border rounded-lg shadow-sm">
@@ -23,7 +22,6 @@ export default function DashboardRoutes() {
           ))}
         </section>
         <section>
-          <h3 className="text-lg font-semibold mb-4">Edit</h3>
           {editRoutes.map((route) => (
             <Link key={route.name} href={route.path}>
               <div className="mb-6 flex flex-col items-center justify-center p-4 border rounded-lg shadow-sm">
@@ -37,7 +35,6 @@ export default function DashboardRoutes() {
           ))}
         </section>
         <section>
-          <h3 className="text-lg font-semibold mb-4">Delete</h3>
           {deleteRoutes.map((route) => (
             <Link key={route.name} href={route.path}>
               <div className="mb-6 flex flex-col items-center justify-center p-4 border rounded-lg shadow-sm">
