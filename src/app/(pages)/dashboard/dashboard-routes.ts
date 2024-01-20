07797c5bc568eccde0076a171957306ba20b7262
inf <decimatedxd@gmail.com>
2024-01-20 11:28:46 +0100

    Add descriptions to dashboard routes

--- src/app/(pages)/dashboard/dashboard-routes.ts
+++ src/app/(pages)/dashboard/dashboard-routes.ts
@@ -2,14 +2,17 @@ export const addRoutes = [
   {
     path: "/dashboard/add-artist",
     name: "Add Artist",
+    description: "Add a new artist to the database.",
   },
   {
     path: "/dashboard/add-track",
     name: "Add Track",
+    description: "Add a new track to the database.",
   },
   {
     path: "/dashboard/add-player-track",
     name: "Add Player Track",
+    description: "Add a new player track to the database.",
   },
 ];
 
@@ -17,18 +20,22 @@ export const editRoutes = [
   {
     path: "/dashboard/edit-artist",
     name: "Edit Artist",
+    description: "Edit an existing artist in the database.",
   },
   {
     path: "/dashboard/edit-track",
     name: "Edit Track",
+    description: "Edit an existing track in the database.",
   },
   {
     path: "/dashboard/edit-player-track",
     name: "Edit Player Track",
+    description: "Edit an existing player track in the database.",
   },
   {
     path: "/dashboard/update-featured-artist",
     name: "Update Featured Artist",
+    description: "Update the current featured artist.",
   },
 ];
 
@@ -36,13 +43,16 @@ export const deleteRoutes = [
   {
     path: "/dashboard/delete-artist",
     name: "Delete Artist",
+    description: "Remove an artist that is no longer apart of the label.",
   },
   {
     path: "/dashboard/delete-track",
     name: "Delete Track",
+    description: "Remove a track that is no longer needed.",
   },
   {
     path: "/dashboard/delete-player-track",
     name: "Delete Player Track",
+    description: "Remove a player track that is no longer needed.",
   },
 ];
