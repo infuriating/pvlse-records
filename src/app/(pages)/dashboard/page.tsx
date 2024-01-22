30a98dbaad809ed9616dde32cd831016540da005
infuriating <decimatedxd@gmail.com>
2024-01-22 14:15:44 +0100

    Refactor return statement in dashboard page component

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import DashboardRoutes from "./components/DashboardRoutes";
 
 export default function page() {
-  return (
-    <>
-      <DashboardRoutes />
-    </>
-  );
+  return <DashboardRoutes />;
 }
